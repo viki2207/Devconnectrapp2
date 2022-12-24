11a3f1b7cc506e2b2012dbc0c387ed2a85998939
viki2207 <vidhishah2207@gmail.com>
2022-12-24 19:35:43 +1100

    24decbefore

--- server/middleware/checkObjectId.js
+++ server/middleware/checkObjectId.js
@@ -1,1 +1,15 @@
-//validate the id porper or not
+// validate the objectId from Mongodb.
+const mongoose = require("mongoose");
+
+const checkObjectId = (idToCheck) => (req, res, next) => {
+  // request : which is coming from the client
+  // response : which is going to the client
+  // next : which will be the next request handler.
+  if (!mongoose.Types.ObjectId.isValid(req.params[idToCheck])) {
+    return res.status(400).json({
+      error: "Invalid ObjectId",
+    });
+  } else next();
+};
+
+module.exports = checkObjectId;
--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,11 +1,79 @@
-//create profile ,uupdate profile, retrieve all the details, add, update delete , reterive and educational details
+// router : to handle routing for the specific part. // auth
+// we need to load the express module
 
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+
+const profile = require("../../models/profile");
+const normalize = require("normalize-url");
+const auth = require("../../middleware/auth");
 const router = express.Router();
 
 router.get("/", function (req, res) {
   res.json({
-    message: "Hello from profile",
+    message: "Hello from profile!",
   });
 });
+
+// @route : POST /api/profile
+// @Desc : create or update the profile
+// @access : private(needs token)/// validatin of token
+router.post(
+  "",
+  auth,
+  check("status", "status is required").notEmpty(),
+  check("skills", "skills is required").notEmpty(),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructure the request
+    const {
+      website,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+    // building the profile object
+
+    console.log(JSON.stringify(req.user));
+    console.log("id value is" + JSON.stringify(req.user.id));
+    const profileFields = {
+      user: req.user.id,
+      website:
+        website && website !== ""
+          ? normalize(website, { forceHttps: true })
+          : "",
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(",").map((skill) => " " + skill.trim()),
+      ...rest,
+    };
+    // build the social media object
+    const socialFields = { youtube, linkedin, facebook, twitter, instagram };
+    profileFields.social = socialFields;
+
+    // start adding the details to mongodb via mongoose.
+    try {
+      console.log("before the profile creation");
+      let profileResult = await profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
+      console.log("profile Result: " + profileResult);
+      return res.status(201).json(profileResult);
+    } catch (err) {
+      console.log("error details: " + err.message);
+    }
+  }
+);
 module.exports = router;
